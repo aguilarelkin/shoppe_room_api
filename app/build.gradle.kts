bda75a7cf57fd1cdf1df2c87753124ffe5ac43b9
aguilarelkin <aguilar.elkin99@gmail.com>
2025-04-11 12:34:30 -0500

    config google services

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.androsh.shopee"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 31
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
